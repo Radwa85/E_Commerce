d5db63bbc54086163884af2c2c6b60b9313ffe18
abdo-essam <abdo.e.mosa@gmail.com>
2026-01-06 22:20:48 +0200

    Add AppStrings class for static string constants

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,81 @@
+abstract class AppStrings {
+  // Auth Screens
+  static const String loginTitle = 'Login to your account';
+  static const String loginSubtitle = 'It\'s great to see you again.';
+  static const String registerTitle = 'Create an account';
+  static const String registerSubtitle = 'Let\'s create your account.';
+
+  // Form Labels
+  static const String fullName = 'Full Name';
+  static const String userName = 'User Name';
+  static const String password = 'Password';
+  static const String confirmPassword = 'Confirm Password';
+
+  // Form Hints
+  static const String enterFullName = 'Enter your full name';
+  static const String enterEmail = 'Enter your email address';
+  static const String enterPassword = 'Enter your password';
+
+  // Buttons
+  static const String signIn = 'Sign In';
+  static const String createAccount = 'Create Account';
+  static const String addToCart = 'Add to Cart';
+  static const String goToCheckout = 'Go To Checkout';
+
+  // Footer Links
+  static const String dontHaveAccount = 'Don\'t have an account?';
+  static const String alreadyHaveAccount = 'Already have an account?';
+  static const String join = 'Join';
+  static const String logIn = 'Log In';
+
+  // Home Screen
+  static const String discover = 'Discover';
+  static const String searchHint = 'Search for clothes...';
+
+  // Categories
+  static const String all = 'All';
+  static const String tshirts = 'Tshirts';
+  static const String jeans = 'Jeans';
+  static const String shoes = 'Shoes';
+
+  // Product Details
+  static const String details = 'Details';
+  static const String price = 'Price';
+  static const String reviews = 'reviews';
+
+  // Cart Screen
+  static const String myCart = 'My Cart';
+  static const String subTotal = 'Sub-total';
+  static const String vat = 'VAT (%)';
+  static const String shippingFee = 'Shipping fee';
+  static const String total = 'Total';
+
+  // Account Screen
+  static const String account = 'Account';
+  static const String myOrders = 'My Orders';
+  static const String myDetails = 'My Details';
+  static const String addressBook = 'Address Book';
+  static const String faqs = 'FAQs';
+  static const String helpCenter = 'Help Center';
+  static const String logout = 'Logout';
+
+  // Address Screen
+  static const String address = 'Address';
+  static const String savedAddress = 'Saved Address';
+  static const String defaultLabel = 'Default';
+
+  // Logout Dialog
+  static const String logoutQuestion = 'Logout?';
+  static const String logoutConfirmation = 'Are you sure you want to logout?';
+  static const String yesLogout = 'Yes, Logout';
+  static const String noCancel = 'No, Cancel';
+
+  // Bottom Navigation
+  static const String home = 'Home';
+  static const String cart = 'Cart';
+
+  // Size Labels
+  static const String sizeS = 'Size S';
+  static const String sizeM = 'Size M';
+  static const String sizeL = 'Size L';
+}
