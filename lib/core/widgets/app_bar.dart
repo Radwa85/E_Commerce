45747d8753db53f3f6206e9f8d51053638020f30
abdo-essam <abdo.e.mosa@gmail.com>
2026-01-06 22:40:59 +0200

    Refactor: Move AppColors to constants directory

--- lib/core/widgets/app_bar.dart
+++ lib/core/widgets/app_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../theme/app_colors.dart';
+import '../constants/app_colors.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
