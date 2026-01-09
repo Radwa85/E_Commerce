e6d802e3bd0939e7a3d45d8ffccedfecb1a747ad
Aboud <gh.abd.com.207@gmail.com>
2026-01-09 10:31:20 -0800

    feat: Add product details screen

---
+++ lib/feature/proudctDetails/ProductDetailsScreen.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../core/constants/app_assets.dart';
+import '../../core/constants/app_colors.dart';
+import '../../core/widgets/app_bar.dart';
+import '../../domain/entity/product.dart';
+import 'components/product_details_bottom_bar.dart';
+
+class ProductDetailsScreen extends StatefulWidget {
+  final Product product;
+
+  const ProductDetailsScreen({super.key, required this.product});
+
+  @override
+  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  late Product product;
+
+  @override
+  void initState() {
+    super.initState();
+    product = Product(
+      name: 'Product Name',
+      numberOfReviews: 5,
+      rating: 4.5,
+      price: 19.99,
+      imageUrl: 'https://picsum.photos/600/300',
+      description: 'Product Description',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(title: 'Details'),
+      backgroundColor: AppColors.background,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: double.infinity,
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 12),
+            Text(
+              product.name,
+              style: GoogleFonts.dmSans(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+                height: 1.2,
+                color: AppColors.textPrimary,
+              ),
+            ),
+            SizedBox(height: 13),
+            Row(
+              children: [
+                SvgPicture.asset(AppAssets.iconStar),
+                SizedBox(width: 6),
+                Text(
+                  "${product.rating}/5",
+                  style: GoogleFonts.readexPro(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    height: 1.4,
+                    color: AppColors.textPrimary,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+                SizedBox(width: 4),
+                Text(
+                  "(${product.numberOfReviews} reviews)",
+                  style: GoogleFonts.readexPro(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    height: 1.4,
+                    color: AppColors.textSecondary,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(width: 13),
+            Text(
+              product.description,
+              style: GoogleFonts.dmSans(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                height: 1.4,
+                color: AppColors.textSecondary,
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: ProductDetailsBottomBar(
+        productPrice: product.price,
+        onAddToCartClick: () => {
+          // TODO Handle add to cart
+        },
+      ),
+    );
+  }
+}
