c2e38453963cc8eb28a3589825d778db80545b8d
Aboud <gh.abd.com.207@gmail.com>
2026-01-09 10:30:58 -0800

    feat: Add Product entity

---
+++ lib/domain/entity/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String name;
+  final int numberOfReviews;
+  final double rating;
+  final double price;
+  final String imageUrl;
+  final String description;
+
+  Product({
+    required this.name,
+    required this.numberOfReviews,
+    required this.rating,
+    required this.price,
+    required this.imageUrl,
+    required this.description,
+  });
+}
